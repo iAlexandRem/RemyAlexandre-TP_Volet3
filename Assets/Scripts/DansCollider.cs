2fe77607f02b0550d53c3be80428934c6e21df51
Reichowww <alexthamagicman@gmail.com>
2026-05-11 21:25:30 -0400

    remontée dans trous aléatoires

--- Assets/Scripts/DansCollider.cs
+++ Assets/Scripts/DansCollider.cs
@@ -18,8 +18,8 @@ public class DansCollider : MonoBehaviour
 
     Vector2 positionDepart;
     public DeplacementParFleches deplacement; // Pour gérer l'arrêt de déplacement par flèches, avec le bool peutBouger
-    public Transform pointRetour; // Un point de retour où le joueur revient
-    bool ignoreProchainTrou = false; // Pour ignorer le trigger du trou de sortie
+    public Transform[] pointsRetour; // Un point de retour (trou) où le joueur revient (et les autres seront aléatoires pour les billes et le sucre)
+    bool ignoreProchainTrou = false; // Pour ignorer temporairement le trigger du trou de sortie
 
     AudioSource audioSource;
     public AudioClip vocalMauvaiseDirectionDroite;
@@ -46,7 +46,7 @@ public class DansCollider : MonoBehaviour
 
     void Update()
     {
-        if (SceneManager.GetActiveScene().name == "Mini-Jeu2") // Juste pour essayer une transition "fluide" des limites de position caméra quand on sort du périmètre du labyrinthe
+        if (SceneManager.GetActiveScene().name == "Mini-Jeu2" && LimitesPositionCamera != null) // Juste pour essayer une transition "fluide" des limites de position caméra quand on sort du périmètre du labyrinthe
         {
             LimitesPositionCamera.transform.localScale = Vector3.Lerp(LimitesPositionCamera.transform.localScale, targetScaleLimites, Time.deltaTime * smoothSpeed);
         }
@@ -74,23 +74,33 @@ public class DansCollider : MonoBehaviour
                 if (ignoreProchainTrou)
                 {
                     ignoreProchainTrou = false;
-                    return; // Si on vient juste d'être téléporté, je dois éviter le reste de la fonction en-dessous, pour pas de tombée double
+                    return; // Si on vient juste d'être téléporté, je dois éviter le reste de la fonction en-dessous, pour pas de tombée double au trou de sortie
                 }
                 ignoreProchainTrou = true;
 
                 anim.SetTrigger("estTombee"); // La fourmi tombe dans un trou  
 
-                deplacement.peutBouger = false; // Déplacement bloqué
+                if (CompareTag("Player"))
+                {
+                    deplacement.peutBouger = false; // Déplacement bloqué du joueur
+                }
 
-                Vector2 dir = ((Vector2)collision.transform.position - (Vector2)transform.position).normalized; // Direction d'un vecteur qui pointe de la fourmi vers le trou
-                rb.AddForce(dir * 10f, ForceMode2D.Impulse); // Une force vers le centre du trou
+                Vector2 dir = ((Vector2)collision.transform.position - (Vector2)transform.position).normalized; // Direction d'un vecteur qui pointe du sujet vers le trou
+                if (CompareTag("Sucre"))
+                {
+                    rb.AddForce(dir * 10f, ForceMode2D.Impulse); // Une force vers le centre du trou
+                }
+                else
+                {
+                    rb.AddForce(dir * 40f, ForceMode2D.Impulse); // Une force vers le centre du trou
+                }
 
                 //Debug.Log("Dans Trou");
                 Invoke("ReviensSurface", 0.5f);
             }
 
 
-            if (collision.CompareTag("HorsLabyrinthe") && camAnimator != null) // Quand on s'aventure hors labyrinthe
+            if (collision.CompareTag("HorsLabyrinthe") && camAnimator != null && CompareTag("Player")) // Quand on s'aventure hors labyrinthe
             {
                 camAnimator.SetTrigger("CamZoom"); // Plan rapproché
 
@@ -121,7 +131,7 @@ public class DansCollider : MonoBehaviour
                 //Debug.Log("Hors Trou");
             }
 
-            if (collision.CompareTag("HorsLabyrinthe") && camAnimator != null) // Quand on revient dedans le labyrinthe
+            if (collision.CompareTag("HorsLabyrinthe") && camAnimator != null && CompareTag("Player")) // Quand on revient dedans le labyrinthe
             {
                 camAnimator.SetTrigger("CamDezoom"); // Plan moins rapproché
 
@@ -136,14 +146,14 @@ public class DansCollider : MonoBehaviour
     {
         if (audioSource.isPlaying) return; // Éviter le spam sonore
 
-        if (collision.gameObject.CompareTag("LimiteDroite"))
+        if (collision.gameObject.CompareTag("LimiteDroite") && CompareTag("Player"))
         {
             if (leJeu != null && leJeu.vocalInstructionsTerminees) // Attendre que les instructions soient finies
             {
                 audioSource.PlayOneShot(vocalMauvaiseDirectionDroite); // Je pense qu'on devrait aller à gauche
             }
         }
-        else if (collision.gameObject.CompareTag("LimiteGauche"))
+        else if (collision.gameObject.CompareTag("LimiteGauche") && CompareTag("Player"))
         {
             audioSource.PlayOneShot(vocalMauvaiseDirectionGauche); // Je pense qu'on devrait aller à droite
         }
@@ -151,23 +161,43 @@ public class DansCollider : MonoBehaviour
 
 
 
-    void ReviensSurface()
+    void ReviensSurface() // Téléportation
     {
-        transform.position = pointRetour.position; // On revient au trou zéro en tombant dans les trous
+        if (pointsRetour == null || pointsRetour.Length == 0) // Si le tableau de trous n'existe pas ou est vide
+        {
+            return; // Pas de téléportation
+        }
 
-        transform.rotation = Quaternion.Euler(0f, 0f, 45f); // Différent angle de rotation à la remontée
+        if (CompareTag("Player"))
+        {
+            transform.position = pointsRetour[0].position; // Le joueur revient au trou 0 en tombant dans les trous
 
-        anim.SetTrigger("vaRemonter"); // La fourmi remonte du trou
+            transform.rotation = Quaternion.Euler(0f, 0f, 45f); // Différent angle de rotation à la remontée
 
-        rb.AddForce(new Vector2(-1f, -1f).normalized * 15f, ForceMode2D.Impulse); // Petite force en diagonale sud-ouest
+            rb.AddForce(new Vector2(-1f, -1f).normalized * 60f, ForceMode2D.Impulse); // Petite force en diagonale sud-ouest
+        }
+        else
+        {
+            int index = Random.Range(1, pointsRetour.Length); // Un trou aléatoire (excepté le 0) parmi ceux de l'array
+            transform.position = pointsRetour[index].position; // La chose est transportée à ce trou
+            if (CompareTag("Sucre"))
+            {
+                rb.AddForce(new Vector2(-1f, -1f).normalized * 67f, ForceMode2D.Impulse); // Grosse force hehe
+            }
+        }
+
+        anim.SetTrigger("vaRemonter"); // Le sujet remonte du trou
 
         Invoke("PostRemontee", 0.7f);
     }
 
     void PostRemontee()
     {
-        deplacement.peutBouger = true; // Déplacement permis
-        anim.speed = 5f; // Stress de la fourmi sur le idle
+        if (CompareTag("Player"))
+        {
+            deplacement.peutBouger = true; // Déplacement permis
+            anim.speed = 5f; // Stress de la fourmi sur le idle
+        }
         ignoreProchainTrou = false; // Je réactive le trigger du trou ici, le délai est important, j'ai testé
     }
 
