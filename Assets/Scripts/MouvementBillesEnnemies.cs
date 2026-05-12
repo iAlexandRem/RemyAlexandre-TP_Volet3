910464f8a4c6443999b051e6e83fb80a3487c7d6
Reichowww <alexthamagicman@gmail.com>
2026-05-12 01:07:04 -0400

    perte de vitesse collisions billes

--- Assets/Scripts/MouvementBillesEnnemies.cs
+++ Assets/Scripts/MouvementBillesEnnemies.cs
@@ -7,6 +7,7 @@ public class MouvementBillesEnnemies : MonoBehaviour
     public float forcePoussee;
     public float distanceActivation; // Jusqu'à où la bille détecte le joueur
     public float tempsRebond; // Temps avant de changer de direction après collision
+    private float vitesseActuelle; // Pour les transitions de vitesse
 
     Rigidbody2D rb;
     private Transform joueur;
@@ -16,6 +17,7 @@ public class MouvementBillesEnnemies : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         joueur = GameObject.FindGameObjectWithTag("Player").transform; // Trouve la fourmi
+        vitesseActuelle = vitesse;
     }
 
     void FixedUpdate()
@@ -27,15 +29,17 @@ public class MouvementBillesEnnemies : MonoBehaviour
 
         Vector2 dir; // Direction de déplacement
 
+        Vector2 dirJoueur = ((Vector2)joueur.position - (Vector2)transform.position).normalized; // Direction vers le joueur
+        float dist = Vector2.Distance(transform.position, joueur.position); // Distance au joueur
+
         // Si enfin pas de rebond ET si le joueur est proche 
-        if (timerRebond <= 0 && Vector2.Distance(transform.position, joueur.position) <= distanceActivation)
+        if (timerRebond <= 0 && dist <= distanceActivation)
         {
-            Vector2 dirJoueur = ((Vector2)joueur.position - (Vector2)transform.position).normalized; // Une dir allant de la bille vers le joueur
-            dir = Vector2.Lerp(rb.linearVelocity.normalized, dirJoueur, acceleration * Time.fixedDeltaTime).normalized; // Virage progressif
+            dir = Vector2.Lerp(rb.linearVelocity.normalized, dirJoueur, acceleration * Time.fixedDeltaTime); // Virage progressif vers le joueur
         }
         else
         {
-            dir = rb.linearVelocity.normalized; // Sinon on garde la directon actuelle
+            dir = rb.linearVelocity.normalized; // Sinon on garde la direction actuelle
 
             if (rb.linearVelocity.magnitude < 0.1f) // Si la bille ne bouge presque plus
             {
@@ -44,7 +48,9 @@ public class MouvementBillesEnnemies : MonoBehaviour
             }
         }
 
-        rb.linearVelocity = dir * vitesse; // Vitesse constante
+        vitesseActuelle *= 0.999f; // Perte progressive de vitesse
+        vitesseActuelle = Mathf.Min(vitesseActuelle + acceleration * Time.fixedDeltaTime, vitesse); // Augmente progressivement la vitesseActuelle (accélération), mais la bloque pour ne jamais dépasser la valeur max "vitesse"
+        rb.linearVelocity = dir.normalized * vitesseActuelle; // Application de la vitesse
     }
 
 
@@ -54,7 +60,7 @@ public class MouvementBillesEnnemies : MonoBehaviour
         {
             GameObject[] trous = GameObject.FindGameObjectsWithTag("Trou"); // Ça met tous les gameobjects Trous dans un array [trous]
 
-            GameObject plusProche = trous[0]; // Je veux repérer le trou le plus proche AU MOMENT DE LA COLLISION, on commence par un premier
+            GameObject plusProche = trous[1]; // Je veux repérer le trou le plus proche au moment de la collision, on commence par un premier
             float minDist = Vector2.Distance(collision.transform.position, plusProche.transform.position); // Distance minimale entre le joueur touché et ce trou (qu'on considère proche)
             foreach (GameObject t in trous) // Pour CHAQUE trou (t) dans l'array
             {
@@ -67,7 +73,7 @@ public class MouvementBillesEnnemies : MonoBehaviour
             }
 
             Vector2 dir = ((Vector2)plusProche.transform.position - (Vector2)collision.transform.position).normalized; // Une dir allant du joueur vers le trou
-            collision.rigidbody.linearVelocity = dir * forcePoussee; // La bille pousse le joueur vers le trou!
+            collision.rigidbody.linearVelocity = dir * forcePoussee; // LA BILLE POUSSE LE JOUEUR VERS LE TROU!
         }
 
 
@@ -75,10 +81,12 @@ public class MouvementBillesEnnemies : MonoBehaviour
         {
             timerRebond = tempsRebond; // Reset du countdown
 
+            vitesseActuelle *= 0.3f; // Perte temporaire de vitesse 
+
             float angle = Random.Range(0f, 360f); // Angle aléatoire
             Vector2 dirAleatoire = new Vector2(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad)); // Direction aléatoire
 
-            rb.linearVelocity = dirAleatoire * vitesse; // Vitesse constante selon direction aléatoire
+            rb.linearVelocity = dirAleatoire * rb.linearVelocity.magnitude * 0.3f; // Rebond selon direction aléatoire
         }
     }
 
@@ -88,7 +96,7 @@ public class MouvementBillesEnnemies : MonoBehaviour
         if (fuite.CompareTag("ZoneProtegee")) // Pour que la bille fuit certaines zones dites protégées (DÉBUT, FIN)
         {
             Vector2 dir = ((Vector2)transform.position - fuite.ClosestPoint(transform.position)).normalized; // Une dir allant du point le plus près du collider vers la bille
-            rb.linearVelocity += dir * 50f; // Vitesse de poussée constante selon cette direction de fuite
+            rb.linearVelocity = dir * rb.linearVelocity.magnitude * 1.5f; // Vitesse de poussée constante selon cette direction de fuite
         }
     }
 }
--- Assets/Scripts/SucreBrun.cs
+++ Assets/Scripts/SucreBrun.cs
@@ -1,24 +1,57 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class SucreBrun : MonoBehaviour
 {
     Rigidbody2D rb;
+    Transform joueur;
+
+    bool estPorte = false;
+    Vector2 offset = new Vector2(-1f, 0f);
+
+    public float distancePrise = 1.5f; // distance pour pouvoir ramasser
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        joueur = GameObject.FindGameObjectWithTag("Player").transform;
         Invoke("PetitePoussee", 3f); // Délai
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     void PetitePoussee()
     {
         rb.AddForce(Vector2.down * 17f, ForceMode2D.Impulse); // Poussée automatique vers le bas
+    }
+
+
+    void Update()
+    {
+        if (Keyboard.current != null && Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            Debug.Log("SPACE détecté");
+            float dist = Vector2.Distance(transform.position, joueur.position);
+
+            // 👉 ON PEUT RAMASSER SEULEMENT SI PROCHE
+            if (dist <= distancePrise)
+            {
+                estPorte = !estPorte;
+
+                if (estPorte)
+                {
+                    rb.linearVelocity = Vector2.zero;
+                }
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (estPorte && joueur != null)
+        {
+            Vector2 target = (Vector2)joueur.position + offset;
+            rb.MovePosition(target);
+        }
     }
 }
