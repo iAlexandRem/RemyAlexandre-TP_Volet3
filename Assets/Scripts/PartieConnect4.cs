f40fe547946bc0bd5ffda27a983d9bf2f616a3d2
Reichowww <alexthamagicman@gmail.com>
2026-05-13 23:20:19 -0400

    AAAAAA

--- Assets/Scripts/PartieConnect4.cs
+++ Assets/Scripts/PartieConnect4.cs
@@ -1,10 +1,10 @@
 using UnityEngine;
 
-public class PartieConnect4 : MonoBehaviour
+public class PartieConnect4 : MonoBehaviour // Avec recherches de théorie sur les tableaux (à 2 integers) et boucles...
 {
 
-    private int[,] plateau = new int[6, 7]; // Un array 2D avec 6 rangées et 7 colonnes : plateau[ligne, colonne]
-    private bool tourRouge = true; // Les rouges commencent vraiment
+    private int[,] plateau = new int[6, 7]; // Un array 2D vide avec 6 rangées et 7 colonnes : plateau[rangee, colonne]
+    private bool tourRouge = true; // Les rouges commencent 
 
 
 
@@ -27,10 +27,10 @@ public class PartieConnect4 : MonoBehaviour
         int joueur = tourRouge ? 1 : 2; // Si c'est le tourRouge, joueur = 1, sinon 2
 
         // Si la colonne est pleine jusqu'en haut
-        if (plateau[5, colonne - 1] != 0) // 6e rangée, et colonne, vérifier si ce n'est pas vide
+        if (plateau[5, colonne - 1] != 0) // 6e rangée, et selon colonne, vérifier si ce n'est pas vide
         {
             Debug.Log("Colonne pleine");
-            return; // On arrête le reste de la fonction
+            return; // Pas besoin du reste de la fonction
         }
 
 
@@ -38,10 +38,11 @@ public class PartieConnect4 : MonoBehaviour
         {
             if (plateau[rangee - 1, colonne - 1] == 0) // Si le trou à telle rangée et colonne est vide
             {
+                // On inscrit alors que rangée, colonne = joueur (et -1 car l'index d'un tableau commence par 0)
                 plateau[rangee - 1, colonne - 1] = joueur; // Exemple: plateau[2, 3] = 1
                 Debug.Log((joueur == 1 ? "Rouge" : "Jaune") + " joue en " + rangee + "," + colonne);
 
-                tourRouge = !tourRouge; // C'est le tour de l'adversaire
+                tourRouge = !tourRouge; // C'est le tour de l'adversaire, ça inverse le bool à chaque fois
 
                 if (tourRouge)
                 {
@@ -53,6 +54,21 @@ public class PartieConnect4 : MonoBehaviour
                 }
             }
         }
+
+        if (VerifierVictoire(joueur)) // Si la fonction bool est true, victoire
+        {
+            Debug.Log("Les " + (joueur == 1 ? "Rouges" : "Jaunes") + " ont GAGNÉ !");
+        }
     }
+
+
+    bool VerifierVictoire(int joueur)
+    {
+        
+        return true;
+    }
+
+
+
 }
 
